9e8684b3f562745540851bfc962639726603f687
Takaaki Shirai <lain.op.rai@gmail.com>
2019-06-02 23:32:33 +0900

    Top page with sample suggestion

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		if _, err := fmt.Fprint(w, `<html>
+こんにちは。梅雨の時期の洗濯に悩んでいませんか。<br>
+<a href="https://www.irisplaza.co.jp/media/A13916021914">梅雨時期の洗濯のコツとカビ対策記事</a>を紹介しますのでご覧ください。
+</html>`); err != nil {
+			return
+		}
 	})
 
-	http.ListenAndServe(":80", nil)
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Fatal("failed to listen/serve")
+	}
+	return
 }
